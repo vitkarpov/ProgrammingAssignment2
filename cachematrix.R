1dc2822aed1a7a1212dd99e2c807731e9a76c858
vitkarpov <viktor.s.karpov@gmail.com>
2016-04-04 20:30:37 +0300

    Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Returns a list with set of methods,
+## which could be used to store and get some data from a closure.
+getCachableObject <- function(x = NULL) {
+  cache <- NULL
+  set <- function(y) {
+    x <<- y
+    cache <<- NULL
+  }
+  get <- function() {
+    x
+  }
+  setValueToCache <- function(value) {
+    cache <<- value
+    value
+  }
+  getValueFromCache <- function() {
+    cache
+  }
+  list(set = set, get = get,
+       getValueFromCache = getValueFromCache,
+       setValueToCache = setValueToCache)
 }
 
+## Returns a cachable object, which could be used
+## to store the result of inverting the matrix
+makeCacheMatrix <- function(x = matrix()) {
+  if (ncol(x) != nrow(x)) {
+    message('The matrix should be square!')
+    return(NULL)
+  }
+  getCachableObject(x)
+}
 
-## Write a short comment describing this function
-
+## Returns the result of inverting the matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cache <- x$getValueFromCache()
+  
+  if (!is.null(cache)) {
+    message('The value`s been gotten from cache!')
+    return(cache)
+  }
+  x$setValueToCache(solve(x$get(), ...))
 }
